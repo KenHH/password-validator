8c3e3e34f0f6516d4193100720943c6ef3140c1b
KenHH <kennth_5@hotmail.com>
2018-02-09 22:32:51 -0400

    Stage 2: Next, define and implement at least 3 more rules of your own choosing (e.g. requiring at least 1 special character, at least 1 digit, both upper and lower case). The new rules should be pushed to a branch named “a2-stage2”

--- app/src/main/java/Validator.java
+++ app/src/main/java/Validator.java
@@ -7,9 +7,37 @@ public class Validator {
     int validate(String inputPassW){
         int countPassedRules = 0;  //  A counter for correct rules
 
+        boolean digit=false;
+        boolean upCase=false;
+        boolean lowCase=false;
+        char ch;
+
+        // Check if string contains atleast one uppercase/digit/lowercase and changes values to true if found.
+        for(int i=0;i < inputPassW.length();i++) {
+            ch = inputPassW.charAt(i);
+            if( Character.isDigit(ch)) {
+                digit = true;
+            }
+            else if (Character.isUpperCase(ch)) {
+                upCase = true;
+            }
+            else if (Character.isLowerCase(ch)) {
+                lowCase = true;
+            }
+        }
         if (!inputPassW.equals( "password" ))   // Check to make sure password is not "password"
             countPassedRules++;
-        if( inputPassW.length() >= 8 )           //Check if length of password is >= 8
+
+        if ( inputPassW.length() >= 8 )           //Check if length of password is >= 8
+            countPassedRules++;
+
+        if ( upCase==true )                     //rule 1 extra
+            countPassedRules++;
+
+        if ( lowCase==true )                   //rule 2 extra
+            countPassedRules++;
+
+        if ( digit==true )                    //rule 3 extra
             countPassedRules++;
 
         System.out.print(countPassedRules);     //Prints out the amount of passed Rules
